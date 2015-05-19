1e8e60689016e1af11ed2d1c3e0e28084fae32c8
Aluno <aluno@SL04TTDESK05-2.local>
2015-05-19 09:03:42 -0300

    Initial Commit

---
+++ sizeClassGlauber/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sizeClassGlauber
+//
+//  Created by Aluno on 19/05/15.
+//  Copyright (c) 2015 Glauber. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ sizeClassGlauber/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sizeClassGlauber
+//
+//  Created by Aluno on 19/05/15.
+//  Copyright (c) 2015 Glauber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
